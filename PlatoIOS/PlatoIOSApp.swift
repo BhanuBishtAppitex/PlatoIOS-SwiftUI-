f42f020f7ee9585a998eba3274e56151a7945f05
Applios Studio <solutionsappsoft@gmail.com>
2022-11-28 11:16:17 +0530

    Initial Commit

---
+++ PlatoIOS/PlatoIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatoIOSApp.swift
+//  PlatoIOS
+//
+//  Created by wOOx Technology on 28/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PlatoIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
